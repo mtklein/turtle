b2518543b2e628fe89fb3f29f9375834677eeed6
Mike Klein <mtklein@gmail.com>
2022-06-20 18:10:21 -0500

    separate live field
        growth	10.3ns -> 9.32ns
        lookup	3.61ns -> 2.89ns

--- hash.c
+++ hash.c
@@ -1,23 +1,23 @@
 #include "hash.h"
 #include <assert.h>
 #include <stdbool.h>
-#include <stdint.h>
 #include <stdlib.h>
 
 struct hash {
     int len, mask;
     struct {
-        intptr_t hash;
-        void     *val;
+        int  live, hash;
+        void *val;
     } table[];
 };
 
 static void insert(struct hash *h, int hash, void *val) {
-    assert(h && h->len <= h->mask && hash != 0);
+    assert(h && h->len <= h->mask);
     int const mask = h->mask;
 
     for (int i = hash & mask, round = 0; round <= mask; round++) {
-        if (h->table[i].hash == 0) {
+        if (h->table[i].live == 0) {
+            h->table[i].live = 1;
             h->table[i].hash = hash;
             h->table[i].val  = val;
             h->len++;
@@ -30,7 +30,6 @@ static void insert(struct hash *h, int hash, void *val) {
 }
 
 struct hash* hash_insert(struct hash *h, int hash, void *val) {
-    if (hash == 0) { hash = 1; }
     int const len = h ? h->len    : 0,
               cap = h ? h->mask+1 : 0;
 
@@ -41,8 +40,8 @@ struct hash* hash_insert(struct hash *h, int hash, void *val) {
         grown->mask = new_cap-1;
 
         for (int i = 0; i < cap; i++) {
-            if (h->table[i].hash != 0) {
-                insert(grown, (int)h->table[i].hash, h->table[i].val);
+            if (h->table[i].live) {
+                insert(grown, h->table[i].hash, h->table[i].val);
             }
         }
         assert(grown->len == len);
@@ -56,11 +55,10 @@ struct hash* hash_insert(struct hash *h, int hash, void *val) {
 }
 
 bool hash_lookup(struct hash const *h, int hash, bool(*match)(void *val, void *ctx), void *ctx) {
-    if (hash == 0) { hash = 1; }
     int const mask = h ? h->mask : -1;
 
     for (int i = hash & mask, round = 0; round <= mask; round++) {
-        if (h->table[i].hash == 0) {
+        if (h->table[i].live == 0) {
             return false;
         }
         if (h->table[i].hash == hash && match(h->table[i].val, ctx)) {
