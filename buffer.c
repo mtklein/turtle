6b31c43fd17e3ea91f9c134cd265df5f331dbdc0
Mike Klein <mtklein@gmail.com>
2022-06-22 15:19:44 -0500

    include sizeof *b in cap math
    This keeps all the allocations nice powers of 2.

--- buffer.c
+++ buffer.c
@@ -4,8 +4,8 @@
 
 struct buffer* buffer_grow(struct buffer *b, size_t more) {
     size_t const len = b ? b->len : 0;
-    if (bit_ceil(len) < len+more) {
-        b = realloc(b, sizeof *b + bit_ceil(len+more));
+    if (bit_ceil(sizeof *b + len) < sizeof *b + len+more) {
+        b = realloc(b, bit_ceil(sizeof *b + len+more));
     }
     if (b) {
         b->len = len+more;
