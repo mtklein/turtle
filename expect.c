1e0970b19e23015445e755a23af31dc6f27e8418
Mike Klein <mtklein@gmail.com>
2022-06-22 07:47:17 -0500

    only need this on decl

--- expect.c
+++ expect.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-__attribute__((noreturn))
 void expect_fail(const char* file, int line, const char* msg) {
     fprintf(stderr, "%s:%d expect(%s) failed\n", file, line, msg);
     abort();
