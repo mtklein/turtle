7fd825466066565d15936707c6e22a45f8b71e48
Mike Klein <mtklein@gmail.com>
2022-07-15 09:07:04 -0500

    best balance of codegen across opt/gcc/x86

--- buffer.c
+++ buffer.c
@@ -1,8 +1,14 @@
 #include "buffer.h"
 #include <stdlib.h>
 
+#if defined(__clang__)
+__attribute__((no_sanitize("unsigned-integer-overflow")))
+#endif
+static _Bool not_pow2_or_zero(size_t x) {
+    return x & (x-1);
+}
+
 void* buffer_push_(void *buf, size_t len, size_t sizeof_T) {
-    return len == 0      ? realloc(buf,       sizeof_T)
-        :  len & (len-1) ? buf
-        :                  realloc(buf, 2*len*sizeof_T);
+    return not_pow2_or_zero(len) ? buf
+                                 : realloc(buf, (len ? 2*len : 1) * sizeof_T);
 }
