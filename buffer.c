dd54364e8455ac27bfd16e07cb84671e006a0806
Mike Klein <mtklein@gmail.com>
2022-07-15 08:25:08 -0500

    just as good with no need for no_sanitize

--- buffer.c
+++ buffer.c
@@ -1,11 +1,8 @@
 #include "buffer.h"
 #include <stdlib.h>
 
-__attribute__((no_sanitize("unsigned-integer-overflow")))
-static _Bool is_zero_or_pow2(size_t x) {
-    return (x & (x-1)) == 0;
-}
-
 void* buffer_push_(void *buf, size_t len, size_t sizeof_T) {
-    return is_zero_or_pow2(len) ? realloc(buf, (len ? 2*len : 1) * sizeof_T) : buf;
+    return len == 0      ? realloc(buf,       sizeof_T)
+        :  len & (len-1) ? buf
+        :                  realloc(buf, 2*len*sizeof_T);
 }
