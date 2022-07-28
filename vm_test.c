7511f3d2216e0bd46066bf91b214307c6f3ab5b4
Mike Klein <mtklein@gmail.com>
2022-07-28 14:57:31 -0500

    test with mad

--- vm_test.c
+++ vm_test.c
@@ -13,14 +13,15 @@ int main(void) {
     struct vm_builder *b = vm_builder();
 
     int x = vm_load(b, 1),
-        y = vm_load(b, 2);
-    vm_store(b, 1, vm_add(b, x,y));
+        y = vm_load(b, 2),
+        z = vm_splat(b, 0x3f800000);
+    vm_store(b, 1, vm_mad(b, x,y,z));
 
     struct vm_program *p = vm_compile(b);
     expect(p);
     b = NULL;
 
-    char scratch[(2+2+4+3+1)*16];
+    char scratch[(2+2+1+5+3+1)*16];
     expect(vm_scratch(p) <= sizeof scratch);
 
     float xs[] = {0,1,2,3,4,5,6,7,8},
@@ -28,12 +29,12 @@ int main(void) {
 
     vm_run(p,NULL,len(xs),NULL,xs,ys);
     for (int i = 0; i < len(xs); i++) {
-        expect(equiv(xs[i], 2*(float)i+1));
+        expect(equiv(xs[i], (float)(i * (i+1) + 1)));
     }
 
     vm_run(p,scratch,len(xs),NULL,xs,ys);
     for (int i = 0; i < len(xs); i++) {
-        expect(equiv(xs[i], 3*(float)i+2));
+        expect(equiv(xs[i], (float)((i * (i+1) + 1) * (i+1) + 1)));
     }
 
     free(p);
